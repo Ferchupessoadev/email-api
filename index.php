5079de9bbe20579667207b66e44949b72bd32a83
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-09-08 01:11:17 -0300

    removed vendor folder and 404.php

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    echo "Hello world";
+require 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+try {
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = $_ENV['EMAIL'];
+    $mail->Password = $_ENV['PASSWORD_EMAIL'];
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+    $mail->setFrom($_ENV['EMAIL'], $_ENV['NAME_EMAIL']);
+    $mail->addAddress('fernandomatiaspessoa471@gmail.com', 'Fernando');
+    $mail->isHTML(true);
+    $mail->Subject = 'mail de trabajo';
+    $mail->Body = 'Email body';
+    $mail->send();
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
--- sendemail/env.php
+++
@@ -1,2 +0,0 @@
-<?php
-$token_gmail = 'token';

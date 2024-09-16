5084986f4d6302788ae45a8f44ec888d8ecea822
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-09-16 04:50:45 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -7,7 +7,15 @@ ini_set('display_startup_errors', '0');
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
+// Quitar el encabezado X-Powered-By
+header_remove('X-Powered-By');
+
 // Cors
 header('Access-Control-Allow-Origin: https://ferchudev.com');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}

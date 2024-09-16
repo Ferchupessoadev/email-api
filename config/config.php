fa179c48a68989243941f8810d73d777afaa2db5
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-09-16 04:46:26 -0300

    CORS config

--- config/config.php
+++ config/config.php
@@ -6,3 +6,8 @@ ini_set('display_errors', '0');
 ini_set('display_startup_errors', '0');
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+// Cors
+header('Access-Control-Allow-Origin: https://ferchudev.com');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');

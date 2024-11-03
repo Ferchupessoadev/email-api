e0854333b86afe1b9090f9c5011ba3190f50a8d5
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-11-03 17:44:30 -0300

    fixed bug with options

--- config/config.php
+++ config/config.php
@@ -25,3 +25,8 @@ if (in_array($origin, $allowed_origins)) {
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
 }
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
